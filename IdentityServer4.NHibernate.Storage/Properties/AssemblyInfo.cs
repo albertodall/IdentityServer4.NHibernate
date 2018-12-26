f7d61537672ecc7ecc76c6a6fccabefbd1dc8f14
Alberto Dallagiacoma <alberto.dallagiacoma@live.it>
2018-12-26 19:29:27 +0100

    Add AssemblyInfo files to manually manage versioning.

---
+++ IdentityServer4.NHibernate.Storage/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Alberto Dallagiacoma")]
+[assembly: AssemblyDescription("NHibernate persistence layer for IdentityServer4")]
+[assembly: AssemblyProduct("IdentityServer4.NHibernate")]
+[assembly: AssemblyTitle("IdentityServer4.NHibernate.Storage")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
