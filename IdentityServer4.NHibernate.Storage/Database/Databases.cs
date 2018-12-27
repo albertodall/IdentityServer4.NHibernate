ea7db685b3eba461fcea9985fcbf2ec9dff419b9
Alberto Dallagiacoma <alberto.dallagiacoma@live.it>
2018-12-27 23:05:36 +0100

    Every database configuration has a parameter to enable console logging of generated SQL statements (default is false)

--- IdentityServer4.NHibernate.Storage/Database/Databases.cs
+++ IdentityServer4.NHibernate.Storage/Database/Databases.cs
@@ -6,6 +6,7 @@ using NHibernate.Driver;
 namespace IdentityServer4.NHibernate.Database
 {
     using global::NHibernate.Cfg;
+    using global::NHibernate.Cfg.Loquacious;
 
     /// <summary>
     /// NHibernate configurations for supported databases.
@@ -16,7 +17,7 @@ namespace IdentityServer4.NHibernate.Database
         /// Database configuration for SQL Server 2008 as backing storage.
         /// </summary>
         /// <returns></returns>
-        public static Configuration SqlServer2008()
+        public static Configuration SqlServer2008(bool showGeneratedSql = false)
         {
             var cfg = new Configuration();
             cfg.Proxy(p => p.ProxyFactoryFactory<StaticProxyFactoryFactory>());
@@ -25,8 +26,11 @@ namespace IdentityServer4.NHibernate.Database
                 db.Dialect<MsSql2008Dialect>();
                 db.Driver<Sql2008ClientDriver>();
                 db.BatchSize = 100;
-                db.LogFormattedSql = true;
                 db.PrepareCommands = true;
+                if (showGeneratedSql)
+                {
+                    EnableSqlLogging(db);
+                }
             });
             return cfg;
         }
@@ -34,7 +38,7 @@ namespace IdentityServer4.NHibernate.Database
         /// <summary>
         /// Database configuration for SQL Server 2012+ as backing storage.
         /// </summary>
-        public static Configuration SqlServer2012()
+        public static Configuration SqlServer2012(bool showGeneratedSql = false)
         {
             var cfg = new Configuration();
             cfg.Proxy(p => p.ProxyFactoryFactory<StaticProxyFactoryFactory>());
@@ -43,8 +47,11 @@ namespace IdentityServer4.NHibernate.Database
                 db.Dialect<MsSql2012Dialect>();
                 db.Driver<Sql2008ClientDriver>();
                 db.BatchSize = 100;
-                db.LogFormattedSql = true;
                 db.PrepareCommands = true;
+                if (showGeneratedSql)
+                {
+                    EnableSqlLogging(db);
+                }
             });
             return cfg;
         }
@@ -52,7 +59,7 @@ namespace IdentityServer4.NHibernate.Database
         /// <summary>
         /// Database configuration for SQLite as backing storage.
         /// </summary>
-        public static Configuration SQLite()
+        public static Configuration SQLite(bool showGeneratedSql = false)
         {
             var cfg = new Configuration();
             cfg.Proxy(p => p.ProxyFactoryFactory<StaticProxyFactoryFactory>());
@@ -61,8 +68,11 @@ namespace IdentityServer4.NHibernate.Database
                 db.Dialect<SQLiteDialect>();
                 db.Driver<SQLite20Driver>();
                 db.BatchSize = 100;
-                db.LogFormattedSql = true;
                 db.PrepareCommands = true;
+                if (showGeneratedSql)
+                {
+                    EnableSqlLogging(db);
+                }
             });
             return cfg;
         }
@@ -71,11 +81,21 @@ namespace IdentityServer4.NHibernate.Database
         /// Database configuration for in-memory SQLite as backing storage.
         /// </summary>
         /// <returns></returns>
-        public static Configuration SQLiteInMemory()
+        public static Configuration SQLiteInMemory(bool showGeneratedSql = false)
         {
             return SQLite()
                 .UsingConnectionString("FullUri=file:memorydb.db?mode=memory&cache=shared")
                 .SetProperty("connection.release_mode", "on_close");
+        }
+
+        /// <summary>
+        /// Enables logging of generated SQL statements.
+        /// </summary>
+        /// <param name="db">Database integration configuration.</param>
+        private static void EnableSqlLogging(IDbIntegrationConfigurationProperties db)
+        {
+            db.LogSqlInConsole = true;
+            db.LogFormattedSql = true;
         }
     }
 }
