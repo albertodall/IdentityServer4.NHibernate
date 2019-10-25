ba3116af1a7d681ad8d78f3fd948db1985e83e57
Alberto Dallagiacoma <a.dallagiacoma@isolutions.it>
2019-10-25 10:01:40 +0200

    Modified schema export script

--- Export-DatabaseSchemas.ps1
+++ Export-DatabaseSchemas.ps1
@@ -24,9 +24,18 @@ $opStoreOptions = New-Object IdentityServer4.NHibernate.Options.OperationalStore
 $bindingFlags= [Reflection.BindingFlags] "Public,Static"
 $exportableConfigurations = [IdentityServer4.NHibernate.Database.Databases].GetMethods($bindingFlags) `
     | Where-Object { ($_.Name -inotlike '*equals*') -and ($_.Name -inotlike '*memory*')}
-$exportableConfigurations | ForEach-Object {
-    $fileName = $_.Name
-    $currentConfiguration = $_.Invoke($null, $null)
-    Write-Host "Creating script $fileName.sql in $OutputPath..."
-    [IdentityServer4.NHibernate.Database.Schema.ScriptCreator]::CreateSchemaScriptForDatabase("$OutputPath\$fileName.sql", $currentConfiguration, $configStoreOptions, $opStoreOptions)
+if ($exportableConfigurations -eq $null) {
+    Write-Host "No database configurations found in assembly $PublishPath\IdentityServer4.NHibernate.Storage.dll"
+} else {
+    Write-Host Found ($exportableConfigurations | Measure-Object).Count configurations
+
+    $exportableConfigurations | ForEach-Object {
+        $fileName = $_.Name
+        $currentConfiguration = $_.Invoke($null, $null)
+        Write-Host Exporting script for $currentConfiguration...
+        Write-Host "Creating script $fileName.sql in $OutputPath..."
+        [IdentityServer4.NHibernate.Database.Schema.ScriptCreator]::CreateSchemaScriptForDatabase("$OutputPath\$fileName.sql", $currentConfiguration, $configStoreOptions, $opStoreOptions)
+    }
 }
+
+
